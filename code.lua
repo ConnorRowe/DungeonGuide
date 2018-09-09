ef2a185398e7ec436a1f0918d9e44fc87729bc4d
ConnorRowe <connormagson@hotmail.co.uk>
2018-09-10 00:50:14 +0100

    Found out how to execute chat commands

--- code.lua
+++ code.lua
@@ -1,6 +1,19 @@
 -- This file is loaded from "DungeonGuide.toc"
 
-SLASH_DG1, SLASH_DG2 = '/dg', '/dungeonguide';
-function SlashCmdList.DG(msg, editbox)
- print("Dungeon Guide is ACTIVE! ");
+local function DungeonGuideCommands(msg, editbox)
+
+  if msg == "deadmines" then
+	  print("DG activating " .. msg)
+	  DEFAULT_CHAT_FRAME.editBox:SetText("/way 10 10") ChatEdit_SendText(DEFAULT_CHAT_FRAME.editBox, 0) 
+	  
+  elseif msg == "wailingcaverns" then
+	print("DG activating " .. msg)
+	
+	else
+	print("Syntax: /dg instance");
+	end
 end
+
+SLASH_DG1, SLASH_DG2 = '/dg', '/dungeonguide';
+
+SlashCmdList["DG"] = DungeonGuideCommands

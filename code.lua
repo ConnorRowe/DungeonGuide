adfd4687063109343a4656b5f461bb6a38f58e69
ConnorRowe <connormagson@hotmail.co.uk>
2018-11-25 00:41:16 +0000

    added HereBeDragons library
    also testing GetLFGDungeonInfo(dungeonId) function

--- code.lua
+++ code.lua
@@ -1,4 +1,5 @@
 -- This file is loaded from "DungeonGuide.toc"
+local hbd = LibStub("HereBeDragons-2.0")
 
 local function SetTomTomCoords(zone, x, y)
 	DEFAULT_CHAT_FRAME.editBox:SetText("/way " .. zone .. x .. " " .. y)
@@ -13,7 +14,16 @@ local function DungeonGuideCommands(msg, editbox)
 	  
   elseif msg == "wailingcaverns" then
 	print("DG activating " .. msg)
-	SetTomTomCoords("Wailing Caverns", 15,15); 
+	SetTomTomCoords("Wailing Caverns", 15,15);
+  
+  elseif msg == "test" then
+	-- local facing = GetPlayerFacing()
+	-- GetPlayerFacing does not work in instances
+	
+	local dungeonID = 1;
+	local name, typeID, subtypeID, minLevel, maxLevel, recLevel, minRecLevel, maxRecLevel, expansionLevel, groupID, textureFilename, difficulty, maxPlayers, description, isHoliday, bonusRepAmount, minPlayers, isTimeWalker, name2, minGearLevel = GetLFGDungeonInfo(dungeonID)
+		
+	print("name: " .. name .. ", name2: " .. name2 .. ", typeID: " .. typeID)
 
 	else
 	print("Syntax: /dg instance");
